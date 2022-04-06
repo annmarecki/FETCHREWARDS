afbc9fefe599966a332f805e79b5ed9de669ca59
Ania Marecki <aniamarecki@gmail.com>
2022-04-05 20:40:56 -0400

    spender routes rough draft

--- fetchRewards/src/api/transactions.js
+++ fetchRewards/src/api/transactions.js
@@ -48,12 +48,55 @@ router.post("/pay", async (req, res, next) => {
       let transactionPayer = await User.findOne({
         where: { payer: req.body.payer },
       });
-      let newTotal = transactionPayer.points + req.body.points;
-      transactionPayer.points = newTotal;
-      await transactionPayer.save();
-      res.send(transaction);
+      await transactionPayer.increment("points", {
+        by: transaction.points,
+      });
+      res.json(transaction);
     } else {
       res.send("Sorry the transaction type is not pay!");
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/spend", async (req, res, next) => {
+  try {
+    if (req.body.transactionType === "spend") {
+      let transaction = await Transaction.create(req.body);
+      let transactionPoints = req.body.points;
+      let payers = await User.findAll({ order: [["timestamp", "DESC"]] });
+      let transactionPayers = [];
+      for (let i = 0; i < payers.length; i++) {
+        let oldest = payers[i];
+        if (transactionPoints > 0) {
+          if (transactionPoints > oldest.points) {
+            transactionPoints -= oldest.points;
+            let decrementUser = await User.findOne({
+              where: { id: oldest.id },
+            });
+            decrementUser.decrement("points", { by: oldest.points });
+            transactionPayers.push({
+              payer: oldest.payer,
+              points: `-${oldest.points}`,
+            });
+          } else {
+            oldest.points -= transactionPoints;
+            let decrementUser = await User.findOne({
+              where: { id: oldest.id },
+            });
+            decrementUser.decrement("points", { by: transactionPoints });
+            transactionPayers.push({
+              payer: oldest.payer,
+              points: `-${transactionPoints}`,
+            });
+            transactionPoints = 0;
+          }
+        }
+        res.send(transactionPayers);
+      }
+    } else {
+      res.send("Sorry the transaction type is not spend!");
     }
   } catch (error) {
     next(error);
